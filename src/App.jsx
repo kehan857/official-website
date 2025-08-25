9294c75b09607f42fc64eaac74c34c5e801fc4dc
kehan857 <kehan857@gmail.com>
2025-08-25 14:24:02 +0800

    feat: 删除HeroSection首页横幅模块
    - 从App.jsx中移除HeroSection组件引用
    - 删除红框中的主要标题、副标题和行动按钮区域
    - 简化页面结构，直接展示产品内容

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from './components/Header/Header'
-import HeroSection from './components/Hero/HeroSection'
 import ProductShowcase from './components/ProductShowcase/ProductShowcase'
 import Workflow from './components/Workflow/Workflow'
 import Features from './components/Features/Features'
@@ -16,7 +15,6 @@ function App() {
     <div className="min-h-screen bg-background">
       <Header />
       <main>
-        <HeroSection />
         <ProductShowcase />
         <Workflow />
         <Features />
