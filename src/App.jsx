148418853e9bed4dfd8149482fa0da444ba6788c
kehan857 <kehan857@gmail.com>
2025-08-30 13:35:02 +0800

    删除ProductMatrix模块：移除AI智能营销解决方案矩阵模块

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import HeroSection from './components/Hero/HeroSection'
-import ProductMatrix from './components/ProductMatrix/ProductMatrix'
+
 import ProductShowcase from './components/ProductShowcase/ProductShowcase'
 import ServiceProcess from './components/ServiceProcess/ServiceProcess'
 import Workflow from './components/Workflow/Workflow'
@@ -23,7 +23,6 @@ function App() {
       <Header />
       <main>
         <HeroSection />
-        <ProductMatrix />
         <Features />
         <CaseStudies />
         <ProductShowcase />
